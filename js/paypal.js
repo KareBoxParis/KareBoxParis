d2eb142fd256be7845be1064d71394768a1d730d
KareBoxParis <karebox@outlook.fr>
2026-07-22 11:27:23 +0200

    Add PayPal subscription button functionality

--- js/paypal.js
+++ js/paypal.js
@@ -1,1 +1,32 @@
+paypal.Buttons({
 
+createSubscription:function(data,actions){
+
+return actions.subscription.create({
+
+plan_id:
+"TON_PLAN_PAYPAL"
+
+});
+
+},
+
+
+onApprove:function(data){
+
+console.log(
+data.subscriptionID
+);
+
+
+alert(
+"Bienvenue chez KareBoxParis"
+);
+
+
+}
+
+
+}).render(
+"#paypal-button"
+);
